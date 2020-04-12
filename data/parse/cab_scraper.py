5023478ed14f6948e52f4e0f6cececf46a536de7
Nick Keirstead <nkeirstead@icloud.com>
2020-04-12 13:32:28 -0400

    CAB login progress

--- data/parse/cab_scraper.py
+++ data/parse/cab_scraper.py
@@ -1,5 +1,13 @@
 import json
 import requests
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+
+USERNAME = ""
+PASSWORD = ""
 
 class CourseInfo:
     def __init__(id, name, prof, semester, prereqs):
@@ -20,13 +28,43 @@ class CourseInfo:
     def getPrereqs():
         return self.prereqs
         
-cs_request_object = {
+cs_request = {
     "other": {"srcdb": "201640"},
     "criteria": [
         {"field": "keyword", "value": "calculus"},
         {"field": "dept", "value": "MATH"}
     ]
 }      
-test_request_string = json.dumps(cs_request_object)
-test_info = requests.get('https://cab.brown.edu/course-search/api/?page=fose&route=search', params=cs_request_object)
+json_request = json.dumps(cs_request)
+
+
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://cab.brown.edu/#")
+login_button = driver.find_element_by_class_name('anon-only')
+first_window = driver.current_window_handle
+login_button.click()
+
+windows = driver.window_handles
+for window in windows:
+    if (window != first_window):
+        driver.switch_to.window(window)
+        
+driver.find_element_by_id('username').send_keys(USERNAME)
+driver.find_element_by_id('password').send_keys(PASSWORD)
+driver.find_element_by_name('_eventId_proceed').click()
+
+WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.ID, "duo_iframe")))
+driver.switch_to.frame("duo_iframe")
+driver.find_element_by_xpath("//button[normalize-space()='Send Me a Push']").click()
+
+driver.switch_to.window(first_window)
+WebDriverWait(driver, timeout=20).until(EC.presence_of_element_located((By.CLASS_NAME, "authed-only")))
+print(driver.find_element_by_class_name("user-name").text)
+
+# Might need to wait until DUO push is completed on phone
+
+test_info = requests.post('https://cab.brown.edu/course-search/api/?page=fose&route=search', data=json_request)
 print(test_info)
