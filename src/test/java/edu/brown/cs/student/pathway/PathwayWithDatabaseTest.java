6359460c198fc091efb4d27faab8a997aeb759d0
Ifechi <ifechi_ilozor@brown.edu>
2020-04-20 19:02:13 -0400

    just left test so that you can see outputs upon mvn package

--- src/test/java/edu/brown/cs/student/pathway/PathwayWithDatabaseTest.java
+++ src/test/java/edu/brown/cs/student/pathway/PathwayWithDatabaseTest.java
@@ -29,17 +29,17 @@ public class PathwayWithDatabaseTest {
     }
   }
 
-//  @Test
-//  public void csConcentrationTest() {
-//    String tablename = "computerscienceba";
-//    List<Integer> reqsTmp = cache.getRequirements(tablename + "_rules");
-//    int[] reqs = reqsTmp.stream().mapToInt(i->i).toArray();
-//
-//    Set<Node> courseSet = cache.getConcentrationCourses(tablename);
-//    Pathway pathwayMaker = new Pathway(reqs, courseSet);
-//    pathwayMaker.makePathway(new HashSet<Node>(), 1, false, "med");
-//    this.pathwayPrinter(pathwayMaker.getPath());
-//  }
+  @Test
+  public void csConcentrationTest() {
+    String tablename = "computerscienceba";
+    List<Integer> reqsTmp = cache.getRequirements(tablename + "_rules");
+    int[] reqs = reqsTmp.stream().mapToInt(i->i).toArray();
+
+    Set<Node> courseSet = cache.getConcentrationCourses(tablename);
+    Pathway pathwayMaker = new Pathway(reqs, courseSet);
+    pathwayMaker.makePathway(new HashSet<Node>(), 1, false, "med");
+    this.pathwayPrinter(pathwayMaker.getPath());
+  }
 
 
   @Test
