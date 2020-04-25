1f2fe352b64f5b6b3407dca5f20f242691ea80cd
Nick Keirstead <nkeirstead@icloud.com>
2020-04-25 19:49:33 -0400

    Three pathway multithreading works

--- src/main/java/edu/brown/cs/student/pathway/PathwayMaker.java
+++ src/main/java/edu/brown/cs/student/pathway/PathwayMaker.java
@@ -1,31 +1,37 @@
 package edu.brown.cs.student.pathway;
 
+import edu.brown.cs.student.main.Database;
+import edu.brown.cs.student.main.DatabaseCache;
+
+import java.sql.SQLException;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.Random;
 
 /**
  * A class to test multithreading in generating multiple pathways for a concentration request.
  */
 public class PathwayMaker {
+  private String concentrationName;
   private int[] reqs;
-  private Set<Node> courseSet, coursesTaken;
+  private Set<Node> coursesTaken;
   private int risingSemester;
-  private boolean aggressive;
   private class PathwayThread extends Thread {
+    private DatabaseCache dbc;
     private int[] reqsClone;
     private Set<Node> courseSetClone, coursesTakenClone;
     private String workload;
     private Pathway pathway;
+    private boolean aggressive;
 
-    /* Right now, the problem is the threads conflict because they're modifying the same
-    reqs, courseSet, and coursesTaken objects --> need to clone each one and pass clones to
-    each thread --> need to override clone() for Node
-     */
-    PathwayThread(String workload) {
+    PathwayThread(String workload) throws SQLException {
+      this.dbc = new DatabaseCache(new Database("data/coursesDB.db"));
       this.reqsClone = reqs.clone();
-      this.courseSetClone = courseSet; // need to deep clone somehow
-      this.coursesTakenClone = coursesTaken; // need to deep clone
+      this.courseSetClone = dbc.getConcentrationCourses(concentrationName);
+      this.coursesTakenClone = copySet(coursesTaken);
       this.workload = workload;
+      this.aggressive = new Random().nextBoolean();
       this.pathway = new Pathway(reqsClone, courseSetClone);
     }
     @Override
@@ -33,7 +39,8 @@ public class PathwayMaker {
       pathway.makePathway(coursesTakenClone, risingSemester, aggressive, workload);
     }
     public void printPathway() {
-      System.out.println("Workload: " + this.workload);
+      System.out.println("Concentration: " + concentrationName + ", Workload: " + this.workload
+          + ", aggressive: " + this.aggressive);
       System.out.println("---------");
       List<Semester> path = pathway.getPath();
       for (Semester list : path) {
@@ -45,15 +52,36 @@ public class PathwayMaker {
         System.out.println();
       }
     }
+
+    /**
+     * A method that deep copies a set of nodes by iterating through and copying each node.
+     * @param s A set of nodes
+     * @return A deep copy of s
+     */
+    private Set<Node> copySet(Set<Node> s) {
+      Set<Node> cp = new HashSet<>();
+      for (Node n: s) {
+        cp.add(copyNode(n));
+      }
+      return cp;
+    }
+
+    /**
+     * A method that deep copies a course node by retrieving its course id / data again from db.
+     * @param n A node
+     * @return A deep copy of n
+     */
+    private Node copyNode(Node n) {
+      return dbc.getCourseData(n.getId());
+    }
   }
 
-  public PathwayMaker(int[] reqs, Set<Node> courseSet, Set<Node> coursesTaken, int risingSemester,
-                      boolean aggressive) {
+  public PathwayMaker(String concentrationName, int[] reqs, Set<Node> coursesTaken,
+                      int risingSemester) {
+    this.concentrationName = concentrationName;
     this.reqs = reqs;
-    this.courseSet = courseSet;
     this.coursesTaken = coursesTaken;
     this.risingSemester = risingSemester;
-    this.aggressive = aggressive;
   }
 
   /**
@@ -61,20 +89,28 @@ public class PathwayMaker {
    * "lo", one is "med", and one is "hi".
    */
   public void makePathways() {
-    PathwayThread loPath = new PathwayThread("lo");
-    //PathwayThread medPath = new PathwayThread("med");
-    //PathwayThread hiPath = new PathwayThread("hi");
+    PathwayThread loPath, medPath, hiPath;
+    loPath = null;
+    medPath = null;
+    hiPath = null;
+    try {
+      loPath = new PathwayThread("lo");
+      medPath = new PathwayThread("med");
+      hiPath = new PathwayThread("hi");
+    } catch (SQLException sqe) {
+      sqe.printStackTrace();
+    }
 
     loPath.start();
-    //medPath.start();
-    //hiPath.start();
+    medPath.start();
+    hiPath.start();
     try {
       loPath.join();
-      //medPath.join();
-      //hiPath.join();
+      medPath.join();
+      hiPath.join();
       loPath.printPathway();
-      //medPath.printPathway();
-      //hiPath.printPathway();
+      medPath.printPathway();
+      hiPath.printPathway();
     } catch (InterruptedException ie) {
       ie.printStackTrace();
     }
--- src/test/java/edu/brown/cs/student/pathway/PathwayTest/MultiPathwayTest.java
+++ src/test/java/edu/brown/cs/student/pathway/PathwayTest/MultiPathwayTest.java
@@ -2,15 +2,14 @@ package edu.brown.cs.student.pathway.PathwayTest;
 
 import edu.brown.cs.student.main.Database;
 import edu.brown.cs.student.main.DatabaseCache;
-import edu.brown.cs.student.pathway.Node;
 import edu.brown.cs.student.pathway.PathwayMaker;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 /**
  * A class to test multithreading when creating pathways.
@@ -22,15 +21,26 @@ public class MultiPathwayTest {
   public void setUp() throws SQLException {
     cache = new DatabaseCache(new Database("data/coursesDB.db"));
   }
+  @After
+  public void tearDown() {
+    cache = null;
+  }
   @Test
-  public void testThreePathways() throws SQLException {
+  public void testCSPathways() throws SQLException {
     String tablename = "computerscienceba";
     List<Integer> reqsTmp = cache.getRequirements(tablename + "_rules");
     int[] reqs = reqsTmp.stream().mapToInt(i->i).toArray();
-    Set<Node> courseSet = cache.getConcentrationCourses(tablename);
 
-    PathwayMaker pm = new PathwayMaker(reqs, courseSet, new HashSet<>(), 1,
-        false);
+    PathwayMaker pm = new PathwayMaker(tablename, reqs, new HashSet<>(), 1);
+    pm.makePathways();
+  }
+  @Test
+  public void testChemPathways() throws SQLException {
+    String tablename = "chemistryba";
+    List<Integer> reqsTmp = cache.getRequirements(tablename + "_rules");
+    int[] reqs = reqsTmp.stream().mapToInt(i->i).toArray();
+
+    PathwayMaker pm = new PathwayMaker(tablename, reqs, new HashSet<>(), 1);
     pm.makePathways();
   }
 }
